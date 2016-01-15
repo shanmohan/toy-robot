54b8ccd832fd2004b4cbf745fd4ba71c3702c664
Mohan Shanmugarajah <s404853@emirates.com>
2016-01-16 10:05:33 +1100

    CommandTypeEnum type is defined to structure the standard commands

---
+++ ToyRobot/ToyRobot.Service/CommandTypeEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyRobot
+{
+    public enum CommandTypeEnum
+    {
+        PLACE = 1,
+        MOVE = 2,
+        LEFT = 3,
+        RIGHT = 4,
+        REPORT = 5
+    }
+}
